0021b1db2777b2ad18461c0ec967e0091e1acba8
David Loesch <davidloesch1@gmail.com>
2020-01-07 12:25:45 -0800

    have state updating correctly for the fetch, need to manipulate it now

--- blackjack/src/components/Table.js
+++ blackjack/src/components/Table.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import Dealer from './Dealer'
-import Seat from './Seat'
+import Dealer from "./Dealer";
+import Seat from "./Seat";
 import "../App.css";
 
 class Table extends Component {
@@ -8,104 +8,114 @@ class Table extends Component {
     super(props);
     this.state = {
       url: "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6",
-      seats: [{
-          seat: 1,
-          hand: [10],
-          total: 10,
-          bust: true
-      }],
-      players: [      
-          {
+      table: [
+        { seat: 1, enabled: true, hand: [], total: 0, bet: 0, bust: false },
+        { seat: 2, enabled: false, hand: [], total: 0, bet: 0, bust: false },
+        { seat: 3, enabled: false, hand: [], total: 0, bet: 0, bust: false },
+        { seat: 4, enabled: true, hand: [], total: 0, bet: 0, bust: false },
+        { seat: 5, enabled: false, hand: [], total: 0, bet: 0, bust: false },
+        { seat: 6, enabled: false, hand: [], total: 0, bet: 0, bust: false },
+        { seat: 7, enabled: false, hand: [], total: 0, bet: 0, bust: false },
+        {
           seat: "dealer",
+          enabled: true,
           hand: [],
           total: 0,
+          bet: null,
           bust: false
         }
-    ],
+      ],
       account: props.account,
-      deck_id: 'ob10h1bdc8ig',
+      deck_id: "iubvpk54vb1x",
       cardsInPlay: [],
+      deal: [],
+      playerNum: 0,
+      drawnCard: {},
       remaining: 0,
-      shuffled: null,
-      drawnCard: {}
+      shuffled: null
     };
-    this.deal = this.deal.bind(this)
-    // this.componentDidMount = this.componentDidMount.bind(this)
+    this.draw = this.draw.bind(this);
+    this.deal = this.deal.bind(this);
+    // this.componentDidMount = this.componentDidMount.bind(this);
   }
-
-//   componentDidMount() {
-//     fetch(this.state.url)
-//       .then(res => res.json())
-//       .then(result => {
-//         console.log(result)
-//         this.setState({
-//           deck_id: result.deck_id,
-//           shuffled: result.shuffled,
-//           remaining: result.remaining
-//         });
-//       });
+  componentDidMount() {
+    let i = 0;
+    this.state.table.forEach(el => {
+      if (el.enabled === true) {
+        i++;
+      }
+    });
+    this.setState({
+      playerNum: i
+    });
+  }
+//   deal() {
+//     let newDeal = this.state.deal.slice(0)
+//     let newTable = this.state.table.slice(0)
+//       let i = 0
+//       let mod = i % 8
+//       while (newDeal.length > 0) {
+//         if (newTable[mod].enabled === true) {
+//           newTable[mod].hand.push(newDeal.shift());
+//           // newTable[mod].hand.push(newDeal.shift());
+//         }
+//         console.log(mod)
+//         i++;
+//       }
 //   }
-
-//   drawCard() {
+//   draw() {
+//     //this api draws the amount of cards needed to deal the hand.
 //     let url =
 //       "https://deckofcardsapi.com/api/deck/" +
 //       this.state.deck_id +
-//       "/draw/?count=1";
+//       "/draw/?count=" +
+//       this.state.playerNum;
 //     fetch(url)
 //       .then(res => res.json())
 //       .then(res => {
-//         let newArray = this.state.cards.splice(0);
-//         let newCards = res.cards;
-//         console.log(newCards);
-//         console.log(newArray);
-//         newCards.map(el => newArray.push(el));
-//         console.log(newArray);
-//         this.setState({
-//           cards: newArray
-//         });
+//         let newDeal = res.cards
+//         this.setState(
+//           {
+//             deal: newDeal,
+//           }
+//         );
 //       });
 //   }
-    draw() {
-      let numPlayers = this.state.seats.slice(0)
-      console.log(numPlayers)
-    //   numPlayers = numPlayers.push(this.state..splice(0))
-        let totalPlayers = this.state.players.splice(0)
-        numPlayers.map(el => {
-            totalPlayers.unshift(el)
-        })
-      this.setState({
-          players: totalPlayers
-      })
-      let numOfCards = ((this.state.seats.length + 1) * 2)
-      console.log(numOfCards)
-      let url = "https://deckofcardsapi.com/api/deck/" + this.state.deck_id + "/draw/?count=" + numOfCards
+
+  deal() {
+
+  }
+  draw() {
+      let numCards = this.state.playerNum * 2
+      let url = "https://deckofcardsapi.com/api/deck/" + this.state.deck_id + "/draw/?count=" + numCards
       fetch(url)
         .then(res => res.json())
         .then(res => {
-            console.log(res.cards)
-            let drawn = res.cards
-            this.setState({
-                cardsInPlay: drawn
-            })
-        })
-        totalPlayers.map(el, i => {
-
-            this.setState({
-                
+            this.setState((state) => {
+                let deal = res.cards
+                let remaining = res.remaining
+                console.log(state.cardsInPlay)
+                let cardsInPlay = state.cardsInPlay.concat(res.cards)
+                return {
+                    deal,
+                    remaining,
+                    cardsInPlay
+                }
+                // cardsInPlay: newCardsInPlay,
+                // deal: res.cards,
+                // remaining: res.remaining
             })
         })
   }
-
-
   render() {
-    return(
-        <>
-            <h1>Table</h1>
-            <Dealer />
-            <Seat seat={this.state.seats}/>
-            <button onClick={()=> this.draw()}>Deal</button>
-        </>
-    ) 
+    return (
+      <>
+        <h1>Table</h1>
+        <Dealer />
+        <Seat seat={this.state.seats} />
+        <button onClick={() => this.draw()}>Deal</button>
+      </>
+    );
   }
 }
 
